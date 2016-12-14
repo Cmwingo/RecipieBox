0f0714845bbede23d7962d844f0db108c281ae1d
Wingo <cmwingo@gmail.com>
2016-12-14 15:50:34 -0800

    Add Ingredient class

---
+++ Objects/Ingredient.cs
@@ -0,0 +1,226 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace RecipieBox
+{
+  public class Ingredient
+  {
+    private int _id;
+    private string _name;
+    private int _quantity;
+
+    public Ingredient(string Name, int Quantity = 0, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+      _quantity = Quantity;
+    }
+
+    public override bool Equals(System.Object otherIngredient)
+    {
+        if (!(otherIngredient is Ingredient))
+        {
+          return false;
+        }
+        else {
+          Ingredient newIngredient = (Ingredient) otherIngredient;
+          bool idEquality = this.GetId() == newIngredient.GetId();
+          bool nameEquality = this.GetName() == newIngredient.GetName();
+          bool quantityEquality = this.GetQuantity() == newIngredient.GetQuantity();
+          return (idEquality && nameEquality);
+        }
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public int GetQuantity()
+    {
+      return _quantity;
+    }
+    public void SetQuantity(int newQuantity)
+    {
+      _quantity = newQuantity;
+    }
+
+    public static List<Ingredient> GetAll()
+    {
+      List<Ingredient> AllIngredients = new List<Ingredient>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM ingredients;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int ingredientId = rdr.GetInt32(0);
+        string ingredientName = rdr.GetString(1);
+        int quantity = rdr.GetInt32(2);
+        Ingredient newIngredient = new Ingredient(ingredientName, quantity, ingredientId);
+        AllIngredients.Add(newIngredient);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return AllIngredients;
+    }
+
+    public void Update(string newName, int newQuantity)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("UPDATE ingredients SET name = @NewName, quantity = @NewQuantity OUTPUT INSERTED.name, INSERTED.quantity WHERE id = @IngredientId;", conn);
+
+      SqlParameter descParam = new SqlParameter();
+      descParam.ParameterName = "@NewName";
+      descParam.Value = newName;
+
+      SqlParameter quantityParam = new SqlParameter();
+      quantityParam.ParameterName = "@NewQuantity";
+      quantityParam.Value = newQuantity;
+
+
+
+      SqlParameter idParam = new SqlParameter();
+      idParam.ParameterName = "@IngredientId";
+      idParam.Value = this._id;
+
+      cmd.Parameters.Add(descParam);
+      cmd.Parameters.Add(quantityParam);
+      cmd.Parameters.Add(idParam);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while (rdr.Read())
+      {
+        this._name = rdr.GetString(0);
+        this._quantity = rdr.GetInt32(1);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO ingredients (name, quantity) OUTPUT INSERTED.id VALUES (@IngredientName, @IngredientQuantity);", conn);
+
+      SqlParameter nameParam = new SqlParameter();
+      nameParam.ParameterName = "@IngredientName";
+      nameParam.Value = this.GetName();
+
+      SqlParameter quantityParam = new SqlParameter();
+      quantityParam.ParameterName = "@IngredientQuantity";
+      quantityParam.Value = this.GetQuantity();
+
+      cmd.Parameters.Add(nameParam);
+      cmd.Parameters.Add(quantityParam);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM ingredients;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+
+    public static Ingredient Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM ingredients WHERE id = @IngredientId", conn);
+      SqlParameter ingredientIdParameter = new SqlParameter();
+      ingredientIdParameter.ParameterName = "@IngredientId";
+      ingredientIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(ingredientIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      int foundIngredientId = 0;
+      string foundIngredientName = null;
+      int foundIngredientQuantity = 0;
+
+      while(rdr.Read())
+      {
+        foundIngredientId = rdr.GetInt32(0);
+        foundIngredientName = rdr.GetString(1);
+        foundIngredientQuantity = rdr.GetInt32(2);
+      }
+      Ingredient foundIngredient = new Ingredient(foundIngredientName, foundIngredientQuantity, foundIngredientId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundIngredient;
+    }
+
+    public void Delete()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("DELETE FROM ingredients WHERE id = @IngredientId;", conn);
+
+      SqlParameter ingredientIdParameter = new SqlParameter();
+      ingredientIdParameter.ParameterName = "@IngredientId";
+      ingredientIdParameter.Value = this.GetId();
+
+      cmd.Parameters.Add(ingredientIdParameter);
+      cmd.ExecuteNonQuery();
+
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+  }
+}
---
+++ Tests/IngredientTest.cs
@@ -0,0 +1,101 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RecipieBox
+{
+  public class IngredientTest : IDisposable
+  {
+    public IngredientTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=recipie_box_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_EmptyAtFirst()
+    {
+      //Arrange, Act
+      int result = Ingredient.GetAll().Count;
+
+      //Assert
+      Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Test_EqualOverrideTrueForSameName()
+    {
+      //Arrange, Act
+      Ingredient firstIngredient = new Ingredient("Name");
+      Ingredient secondIngredient = new Ingredient("Name");
+
+      //Assert
+      Assert.Equal(firstIngredient, secondIngredient);
+    }
+
+    [Fact]
+    public void Test_Save()
+    {
+      //Arrange
+      Ingredient testIngredient = new Ingredient("Name");
+      testIngredient.Save();
+
+      //Act
+      List<Ingredient> result = Ingredient.GetAll();
+      List<Ingredient> testList = new List<Ingredient>{testIngredient};
+
+      //Assert
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Test_SaveAssignsIdToObject()
+    {
+      //Arrange
+      Ingredient testIngredient = new Ingredient("Name");
+      testIngredient.Save();
+
+      //Act
+      Ingredient savedIngredient = Ingredient.GetAll()[0];
+
+      int result = savedIngredient.GetId();
+      int testId = testIngredient.GetId();
+
+      //Assert
+      Assert.Equal(testId, result);
+    }
+
+    [Fact]
+    public void Test_FindFindsIngredientInDatabase()
+    {
+      //Arrange
+      Ingredient testIngredient = new Ingredient("Name");
+      testIngredient.Save();
+
+      //Act
+      Ingredient result = Ingredient.Find(testIngredient.GetId());
+
+      //Assert
+      Assert.Equal(testIngredient, result);
+    }
+
+    [Fact]
+    public void Test_Update_UpdatesInDb()
+    {
+      Ingredient testIngredient = new Ingredient("Name", 0);
+      testIngredient.Save();
+      testIngredient.Update("Other name", 0);
+
+      Ingredient newIngredient = new Ingredient("Other name", 0, testIngredient.GetId());
+
+      Assert.Equal(testIngredient, newIngredient);
+    }
+
+    public void Dispose()
+    {
+      Ingredient.DeleteAll();
+      Recipie.DeleteAll();
+    }
+  }
+}
